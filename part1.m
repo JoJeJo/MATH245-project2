49863c1b725302d7220aca34ad99ecc2f2141ef2
JoJeJo <109493932+JoJeJo@users.noreply.github.com>
2025-01-30 15:14:59 -0800

    Unnecessarily renamed files

---
+++ part1.m
@@ -0,0 +1,14 @@
+%{
+MATH 245 MATLAB 2 PART 1
+_____________________
+Jerom Jothiprakasam
+jothipra@usc.edu
+
+Natalia Hopper
+nhopper@usc.edu
+_______________________________________
+1/30/2025 Original Jerom Jothiprakasam
+%}
+
+clc;
+clear;
