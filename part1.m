efb2d5ac7edd44bc5875317448add6e5e3429b21
Jerom Jothiprakasam <109493932+JoJeJo@users.noreply.github.com>
2025-02-01 16:26:10 -0800

    Add files via upload

--- part1.m
+++ part1.m
@@ -10,15 +10,15 @@ _______________________________________
 Revision History
 Date            Changes             Programmer
 ---------------------------------------------------------
-01/30/2025      Original        Jerom Jothiprakasam
-02/01/2025     Q1,part(A)         Natalia Hopper
-02/01/2025     Q1,part(B)       Jerom Jothiprakasam
+1/30/2025      Original         Jerom Jothiprakasam
+2/1/2025       Q1,part(A)         Natalia Hopper
+2/1/2025       Q1,part(B)       Jerom Jothiprakasam
 %}
 
 clc;
 clear;
 
-%% part (a)
+%% part (A)
 
 tSpan = [0 10]; % Time interval [0, 10]
 y0 = 2; % Initial Population
@@ -33,22 +33,71 @@ xlabel('P(t) - Population');
 ylabel('t - time');
 title('P(t) versus t');
 
-%% part (b)
+%% part (B)
 
-h = 0.001;               % Time Step
-tFinal = 10;             % Final Time
-timeVector = 0:h:tFinal; % Time Inputs
-N = length(timeVector);  % Number of Time Steps
+h1 = 0.001;                % Time Step
+tFinal = 10;               % Final Time
+timeVector1 = 0:h1:tFinal; % Time Inputs
+N1 = length(timeVector1);  % Number of Time Steps
 
-P = zeros(1, N);
-P(1) = y0;               % Initial condition
+P1 = zeros(1, N1);
+P1(1) = y0;                % Initial condition
 
-for i = 1:N-1
-    P(i+1) = P(i) + h * r * P(i);
+for i = 1:N1-1
+    P1(i+1) = P1(i) + h1 * r * P1(i);
 end
 
 figure;
+hold on;
 plot(timeVector, P, 'r-');
+
+%% part (C)
+
+h2 = 0.01;                 % Time Step
+tFinal = 10;               % Final Time
+timeVector2 = 0:h2:tFinal; % Time Inputs
+N2 = length(timeVector2);  % Number of Time Steps
+
+P2 = zeros(1, N2);
+P2(1) = y0;                % Initial condition
+
+for i = 1:N2-1
+    P2(i+1) = P2(i) + h2 * r * P2(i);
+end
+
+plot(timeVector, P2, 'b-');
+
+h3 = 0.1;                  % Time Step
+tFinal = 10;               % Final Time
+timeVector3 = 0:h3:tFinal; % Time Inputs
+N3 = length(timeVector3);  % Number of Time Steps
+
+P3 = zeros(1, N3);
+P3(1) = y0;                % Initial condition
+
+for i = 1:N3-1
+    P3(i+1) = P3(i) + h3 * r * P3(i);
+end
+
+plot(timeVector, P3, 'g-');
+
+
+h4 = 1;                    % Time Step
+tFinal = 10;               % Final Time
+timeVector4 = 0:h4:tFinal; % Time Inputs
+N4 = length(timeVector4);  % Number of Time Steps
+
+P4 = zeros(1, N4);
+P4(1) = y0;                % Initial condition
+
+for i = 1:N4-1
+    P4(i+1) = P4(i) + h4 * r * P4(i);
+end
+
+plot(timeVector, P4, 'c-');
+
+
 xlabel('Time (t)');
 ylabel('Population (P(t))');
 title('P(t) versus t Using Euler''s Method');
+legend('P(t) h=0.001', 'P(t) h=0.01', 'P(t) h=0.1', 'P(t) h=1');
