ff1caae5941e40603849ec56e9cd05ed2792922b
NataliaHopper <nhopper@usc.edu>
2025-02-09 13:07:34 -0800

    Update part3.m

--- part3.m
+++ part3.m
@@ -15,3 +15,14 @@ Date            Changes             Programmer
 
 clc;
 clear;
+
+%% Part (a)
+
+%{ 
+Initial value problem for number of visitors on the island:
+
+x'(t) = (3)(500) - (10)(x)/(100-5t) - 10(t + 1/(1+t))
+x(0) = 10,000 visitors
+
+%}
+
